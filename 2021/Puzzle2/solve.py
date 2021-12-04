e7df1dce8cfcdb1c8bb2c306627c086b32d80a10
Himanshu Jain <himanshu_jain@trimble.com>
2021-12-04 13:21:50 -0600

    Puzzle 2 : Part 2

--- 2021/Puzzle2/solve.py
+++ 2021/Puzzle2/solve.py
@@ -2,7 +2,7 @@ with open('input_2.txt') as f:
     lines = f.read().splitlines()
 
 
-def get_position():
+def get_position_part1():
     horizontal, depth = 0, 0
     for l in lines:
         instruction = l.split()
@@ -17,7 +17,27 @@ def get_position():
     return horizontal, depth
 
 
-(x, y) = get_position()
+(x, y) = get_position_part1()
 
-print(x*y)
+print('Part 1 :', x*y)
 
+
+def get_position_part_2():
+    horizontal, depth, aim = 0, 0, 0
+    for l in lines:
+        instruction = l.split()
+        command = instruction[0]
+        units = int(instruction[1])
+        if command == 'forward':
+            horizontal += units
+            depth += units * aim
+        elif command == 'down':
+            aim += units
+        elif command == 'up':
+            aim -= units
+    return horizontal, depth, aim
+
+
+(h, d, a) = get_position_part_2()
+
+print('Part 2 :', h*d)
