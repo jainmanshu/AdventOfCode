8afd32469718ebacc622e01b3dbf39edffd1becf
Himanshu Jain <himanshu_jain@trimble.com>
2021-12-12 19:10:04 -0600

    Puzzle 7 : Part 2

--- 2021/Puzzle7/solve.py
+++ 2021/Puzzle7/solve.py
@@ -7,7 +7,7 @@ l = list(map(int, l))
 
 
 def fuel_needed(pos):
-    return sum([abs(x - pos) for x in l])
+    return [abs(x - pos) for x in l]
 
 
 max_pos = max(l)
@@ -15,9 +15,24 @@ max_pos = max(l)
 def least_align_pos():
     fuel_cost = {}
     for i in range(1, max_pos):
-        fuel_cost[i] = fuel_needed(i)
+        fuel_cost[i] = sum(fuel_needed(i))
     return fuel_cost
 
 dict1 = least_align_pos()
 
 print(min(dict1.values()))
+
+
+def fuel_needed_2(pos):
+    dist = fuel_needed(pos)
+    return list(map(lambda n: n * (n+1) / 2, dist))
+
+def least_align_pos_2():
+    fuel_cost = {}
+    for i in range(1, max_pos):
+        fuel_cost[i] = sum(fuel_needed_2(i))
+    return fuel_cost
+
+dict2 = least_align_pos_2()
+
+print(min(dict2.values()))
