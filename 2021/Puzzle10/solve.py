8129d24e2cc086ba378b1ea413c52c3d075e471c
Himanshu Jain <himanshu_jain@trimble.com>
2021-12-18 21:50:33 -0600

    Puzzle10: Part 2

--- 2021/Puzzle10/solve.py
+++ 2021/Puzzle10/solve.py
@@ -1,11 +1,18 @@
 with open('input_10.txt') as f:
     lines = f.read().splitlines()
 
-mapping_score = {
+corrupted_mapping_score = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
+}
+
+autocomplete_mapping_score = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
 }
 
 
@@ -27,16 +34,63 @@ def found_illegal(expr):
             return i
     if len(stack) == 0:
         return None
+    return stack
+
+
+# print(found_illegal('[({(<(())[]>[[{[]{<()<>>'))
 
 def part_1():
     score = 0
+    incomplete = []
     for l in lines:
         out = found_illegal(l)
-        if out is None:
+        if out is None or len(out) > 1:
+            incomplete.append(''.join(out))
             continue
         else:
-            score += mapping_score.get(out)
+            score += corrupted_mapping_score.get(out)
+    return score, incomplete
+
+
+score_1, incomplete = part_1()
+
+print('Part 1:', score_1)
+
+
+def get_close(ch):
+    if ch == '{':
+        return '}'
+    elif ch == '[':
+        return ']'
+    elif ch == '<':
+        return '>'
+    elif ch == '(':
+        return ')'
+
+
+def get_score(expr):
+    score = 0
+    for e in expr:
+        score = score * 5 + autocomplete_mapping_score.get(e)
     return score
 
 
-print('Part 1:', part_1())
+def find_complete(expr):
+    complete_string = ''
+    for e in expr[::-1]:
+        complete_string += get_close(e)
+    return complete_string
+
+
+def part_2():
+    auto_score = []
+    for i in incomplete:
+        complete_string = find_complete(i)
+        score = get_score(complete_string)
+        auto_score.append(score)
+    auto_score.sort()
+    odd_pos = int((len(auto_score) + 1) / 2)
+    return auto_score[odd_pos-1]
+
+
+print('Part 2:', part_2())
