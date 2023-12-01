932b9b2140e2f3ec594e1822aa7e2b91834a753c
Himanshu Jain <jainmanshu@gmail.com>
2023-12-01 16:37:35 -0600

    2023: Puzzle1 (Part-2)

--- 2023/Puzzle1/solve.py
+++ 2023/Puzzle1/solve.py
@@ -1,10 +1,11 @@
+import re
+
 with open('input_2.txt') as f:
     parsed = f.read().splitlines()
 
 def getDigits(inputStr):
     beg = 0
     end = len(inputStr) - 1
-    out = ""
     while end >= beg:
         if not inputStr[beg].isdigit():
             beg += 1
@@ -19,4 +20,33 @@ def solve1():
         sum1 += getDigits(p)
     return sum1
 
-print(solve1())
+print("Puzzle 1:", solve1())
+
+# Need to condider edge cases while converting string to avoid over-writing
+digitMap = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
+
+# convert the string into respective digit and then use solve1
+def convert(str1):
+    tmp1 = str1
+    for k in digitMap.keys():
+        tmp1 = re.sub(k, str(digitMap[k]), tmp1)
+    return tmp1
+
+def solve2():
+    sum1 = 0
+    for p in parsed:
+        parsed_converted = convert(p)
+        sum1 += getDigits(parsed_converted)
+    return sum1
+
+print("Puzzle 2:", solve2())
