976ceb542218e5cc9e5ae477be4c7e6aa3391436
Himanshu Jain <jainmanshu@gmail.com>
2024-12-08 23:11:13 -0600

    2024: Day 5 - Puzzle 5 - Part 1

---
+++ 2024/Puzzle5/solve.py
@@ -0,0 +1,28 @@
+import collections
+with open('input_1.txt') as f:
+    rules, updates = f.read().split('\n\n')
+
+ordering = collections.defaultdict(set)
+
+for rule in rules.split('\n'):
+    n1, n2 = rule.split("|")
+    ordering[n1].add(n2)
+
+def is_right_order(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            if not(arr[j] in ordering[arr[i]] and arr[i] not in ordering[arr[j]]):
+                return False
+    return True
+
+def part1():
+    out = 0
+    for update in updates.split('\n'):
+        update_arr = update.split(',')
+        if is_right_order(update_arr):
+            mid = len(update_arr) // 2
+            out += int(update_arr[mid])
+    return out
+
+print('Part-1: ', part1())
