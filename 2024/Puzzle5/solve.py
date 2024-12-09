e08ab32a0f23736edfd0f388396d5eb796ad21b9
Himanshu Jain <jainmanshu@gmail.com>
2024-12-08 23:29:20 -0600

    2024: Day 5 - Puzzle 5 - Part 2

--- 2024/Puzzle5/solve.py
+++ 2024/Puzzle5/solve.py
@@ -26,3 +26,27 @@ def part1():
     return out
 
 print('Part-1: ', part1())
+
+def make_right_order(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[j] in ordering[arr[i]] and arr[i] not in ordering[arr[j]]:
+                continue
+            else:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+def part2():
+    out = 0
+    for update in updates.split('\n'):
+        update_arr = update.split(',')
+        if is_right_order(update_arr):
+            continue
+        else:
+            right_order = make_right_order(update_arr)
+            mid = len(right_order) // 2
+            out += int(right_order[mid])
+    return out
+
+print('Part-2', part2())
