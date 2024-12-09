eee5400edff6282299c8b66f661d5c7100e2ec1f
Himanshu Jain <jainmanshu@gmail.com>
2024-12-09 01:58:09 -0600

    2024: Day 9 Puzzle 9 Part 2

--- 2024/Puzzle9/solve.py
+++ 2024/Puzzle9/solve.py
@@ -36,6 +36,58 @@ def part1(input):
     
     return out
 
-
-
 print("Part 1: ", part1(disk_map))
+
+def part2(input):
+    arr = []
+
+    N = len(input)
+    curr = 0
+    count = collections.Counter()
+
+    for i in range(N):
+        if i % 2 == 0:
+            for _ in range(int(input[i])):
+                arr.append(curr)
+            count[curr] = int(input[i])
+            curr += 1
+        else:
+            for _ in range(int(input[i])):
+                arr.append(" ")
+    
+    m = curr
+
+    while m > 0:
+        r = count[m]
+
+        if r == 0:
+            m -= 1
+            continue
+
+        spaces = 0
+        for i in range(len(arr)):
+            if arr[i] == m:
+                break
+            if arr[i] == " ":
+                spaces += 1
+            else:
+                spaces = 0
+            
+            if spaces >= r:
+                for j in range(len(arr)):
+                    if arr[j] == m:
+                        arr[j] = " "
+                for j in range(count[m]):
+                    arr[i-j] = m
+                break
+        m -= 1
+
+    out = 0
+
+    for i in range(len(arr)):
+            if arr[i] != " ":
+                out += i * int(arr[i])
+    
+    return out
+
+print("Part 2 : ", part2(disk_map))
